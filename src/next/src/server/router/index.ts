cc45dbf5f48d972966cf5be0c806678ecbc5c477
xrexy <rexydoesimportantthings@gmail.com>
2022-07-19 21:32:40 +0300

    Create new t3 app with all dependencies

--- src/next/src/server/router/index.ts
+++ src/next/src/server/router/index.ts
@@ -2,7 +2,11 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-export const appRouter = createRouter().transformer(superjson);
+import { authRouter } from "./auth";
+
+export const appRouter = createRouter()
+  .transformer(superjson)
+  .merge("auth.", authRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
