b5c3555334b2234753dae6300741611009f07fdb
xrexy <rexydoesimportantthings@gmail.com>
2022-07-19 21:33:37 +0300

    Remove auth router

--- src/next/src/server/router/index.ts
+++ src/next/src/server/router/index.ts
@@ -2,11 +2,7 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { authRouter } from "./auth";
-
-export const appRouter = createRouter()
-  .transformer(superjson)
-  .merge("auth.", authRouter);
+export const appRouter = createRouter().transformer(superjson);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
