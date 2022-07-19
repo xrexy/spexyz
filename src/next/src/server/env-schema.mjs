904ed86316e304f3e70b4b304e631e89154cb0e9
xrexy <rexydoesimportantthings@gmail.com>
2022-07-19 21:43:02 +0300

    Fix env schema

--- src/next/src/server/env-schema.mjs
+++ src/next/src/server/env-schema.mjs
@@ -3,6 +3,6 @@ import { z } from "zod";
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET: z.string().minLength(16),
+  NEXTAUTH_SECRET: z.string().min(16),
   NEXTAUTH_URL: z.string().url(),
 });
