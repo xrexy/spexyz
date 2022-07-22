2219ad52112a7248c3b804c9f5296a0517817194
xrexy <rexydoesimportantthings@gmail.com>
2022-07-22 15:30:07 +0300

    Add redirect trpc route

--- src/next/src/server/router/index.ts
+++ src/next/src/server/router/index.ts
@@ -1,8 +1,11 @@
 // src/server/router/index.ts
 import { createRouter } from "./context";
 import superjson from "superjson";
+import { redirectRouter } from "./redirect";
 
-export const appRouter = createRouter().transformer(superjson);
+export const appRouter = createRouter()
+  .transformer(superjson)
+  .merge("redirects.", redirectRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
